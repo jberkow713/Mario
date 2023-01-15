3188575790357e73f8b2799ed16bfdcdf22f9362
jesse berkowitz <jberkow@brandeis.edu>
2023-01-14 23:10:19 -0800

    movement collision with rectangles and gravity working!

--- Runner.py
+++ Runner.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 from sys import exit
+from pygame.locals import  *
 pygame.init()
 
 WIDTH=800
@@ -12,6 +13,7 @@ font = pygame.font.Font('font.ttf',50)
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 Meteors = []
+Rectangles= []
 
 class Player:
     def __init__(self, x):
@@ -20,21 +22,59 @@ class Player:
         self.image = pygame.image.load('player_walk_1.png').convert_alpha()
         self.rect = self.image.get_rect(bottomright=(self.x,self.y))
         self.speed = 5
+        self.floor = None
         self.can_jump=True
+
     def move(self):
-        if self.can_jump==False:
-            self.y +=5
+        if self.can_jump==True and self.floor!=None:
+            count = 0
+            temp = self.image.get_rect(bottomright=(self.x,self.y+10))
+            for r in Rectangles:
+                if pygame.Rect.colliderect(temp, r.rect)==0:
+                    count+=1
+            if count==len(Rectangles):
+                self.y+=10
+                if self.y>=700:
+                    self.y = 700
+                    self.floor=None
+        if self.can_jump==False:            
+            current = self.y + 10
+            for r in Rectangles:
+                temp = self.image.get_rect(bottomright=(self.x, current))
+                if pygame.Rect.colliderect(temp, r.rect)==1:
+                    self.floor = self.y = r.y
+                    self.can_jump=True
+                    return
+            self.y +=10
             if self.y>=700:
                 self.y = 700
                 self.can_jump=True
+
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            self.x -=self.speed
-        if keys[pygame.K_RIGHT]:
-            self.x +=self.speed
+            current = self.x - self.speed
+            count = 0
+            for r in Rectangles:                            
+                temp = self.image.get_rect(bottomright=(current,self.y))
+                if pygame.Rect.colliderect(temp, r.rect)==0:
+                    count+=1
+            if count ==len(Rectangles):
+                self.x =current                   
+                                                                   
+        if keys[pygame.K_RIGHT]:            
+            current = self.x + self.speed
+            count = 0
+            for r in Rectangles:                            
+                temp = self.image.get_rect(bottomright=(current,self.y))
+                if pygame.Rect.colliderect(temp, r.rect)==0:
+                    count+=1
+            if count ==len(Rectangles):
+                self.x =current                          
+                
         if keys[pygame.K_RETURN] and self.can_jump==True:
-            self.y-= 250
+            self.y -= 300
             self.can_jump=False
+        
         if self.rect.left>=WIDTH:
             self.x = 0
         elif self.rect.right<0:
@@ -50,7 +90,7 @@ class Meteor:
         self.y = 0
         self.speed = 5
         self.image = pygame.image.load('Meteor.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (100, 75))
+        self.image = pygame.transform.scale(self.image, (75, 50))
         self.rect = self.image.get_rect(midtop=(self.x,self.y))
         Meteors.append(self)          
     def blit(self):
@@ -67,9 +107,26 @@ class Meteor:
                         self.rect.x = x
                         rework=True
                         break                    
+# pygame.draw.rect(surface, color, pygame.Rect(30, 30, 60, 60))
+class RECT:
+    def __init__(self,color,x,y, width,height):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height   
+        self.rect = Rect(x, y, width, height)
+        Rectangles.append(self)        
+    def blit(self):
+        pygame.draw.rect(screen, self.color,self.rect)    
+
 for _ in range(5):
     Meteor()
 P = Player(100)
+R = RECT('blue', 250,600,100,100)
+R_2 = RECT('red',350,500,100,100 )
+print(Rectangles)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -77,10 +134,13 @@ while True:
             exit()
     screen.fill(BG_Color)
     screen.blit(ground,(0,700))    
+    for r in Rectangles:
+        r.blit()
     for meteor in Meteors:
         meteor.move()
         meteor.blit()    
     P.move()
-    P.blit()    
+    P.blit()
+
     pygame.display.update()
     clock.tick(60)
