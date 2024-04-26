da17e133ac5c6ceba857f0bb362cfc267c660491
jesse berkowitz <jberkow@brandeis.edu>
2024-04-25 21:43:57 -0400

    added off map falling

--- Jumper.py
+++ Jumper.py
@@ -13,14 +13,19 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 font = pygame.font.Font('font.ttf',50)
 pygame.display.set_caption('Jumper')
 clock = pygame.time.Clock()
+gaps = []
+ground = []
 
 class Ground:
-    def __init__(self, size):
-        self.x = 0
+    def __init__(self, start, size):
+        self.start = start        
         self.size=size
+        gaps.append((self.start,self.start+self.size))
         self.image = pygame.image.load('ground.png')
         self.image = pygame.transform.scale(self.image, (self.size,100))
-        self.rect = self.image.get_rect(topleft=(self.x,700))
+        self.rect = self.image.get_rect(topleft=(self.start,700))
+        ground.append(self)
+
     def blit(self):
         screen.blit(self.image,self.rect)
 class Player:
@@ -32,13 +37,23 @@ class Player:
         self.y_size = 75
         self.speed = 5
         self.can_jump=True
-        self.image = pygame.image.load('player_walk_1.png').convert_alpha()
+        self.image = pygame.image.load('mario.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (75,75))
         self.rect = self.image.get_rect(bottomleft=(self.x,self.y))
     
     def blit(self):
         screen.blit(self.image,self.rect)
     def move(self):
+
+        if self.rect.y >HEIGHT:
+            print('game over')
+            pygame.quit()
+            exit()
+
+        for start,end in gaps:            
+            if self.rect.x <start or self.rect.x>end:                
+                self.rect.y +=10
+
         if self.can_jump==False:
             self.rect.y +=10
             if self.rect.y>=625:
@@ -63,7 +78,8 @@ class Player:
 class Level:
     def __init__(self,player_x, g_size):
         self.player = Player(player_x)
-        self.ground = Ground(g_size)
+        self.ground = Ground(0,g_size)
+        print(gaps)
     def blit(self):
         self.ground.blit()
         self.player.move()
@@ -71,11 +87,11 @@ class Level:
     def rebuild(self):
         # Advanced build function insert here
         if self.player.rect.x>WIDTH-self.player.x_size:
-            size = random.randint(700,1000)            
-            self.ground = Ground(size)
+            size = random.randint(800,1000)            
+            self.ground = Ground(0,size)
             self.player.rect.x = 0    
 
-L = Level(0,700)
+L = Level(0,600)
 
 while True:
     for event in pygame.event.get():
